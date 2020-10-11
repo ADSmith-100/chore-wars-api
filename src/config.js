51f6a1ee1c15e4f915384aebc556ac94ffd514e7
Adam Smith <coloradoasmith@gmail.com>
2020-10-11 17:18:31 -0600

    mapping out server structure

--- src/config.js
+++ src/config.js
@@ -1,6 +1,8 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || "development",
+  DB_URL:
+    process.env.DB_URL || "postgresql://dunder_mifflin@localhost/blogful-auth", //this will change!
+  JWT_SECRET: process.env.JWT_SECRET || "whoof-arted", //does this matter?
+  JWT_EXPIRY: process.env.JWT_EXPIRY || "3h",
 };
-
-//We can add API_TOKENS and other secrets to this file in the future if and when needed.
