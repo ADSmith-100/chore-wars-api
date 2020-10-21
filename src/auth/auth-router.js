c2c00cf030f84c3b4ec2b4e61f1812d5a70ebec8
Adam Smith <coloradoasmith@gmail.com>
2020-10-20 22:30:03 -0600

    changes JWT payload to user_id

--- src/auth/auth-router.js
+++ src/auth/auth-router.js
@@ -32,7 +32,7 @@ authRouter.post("/login", jsonBodyParser, (req, res, next) => {
           });
 
         const sub = dbUser.email;
-        const payload = { email: dbUser.email };
+        const payload = { user_id: dbUser.id };
         res.send({
           authToken: AuthService.createJwt(sub, payload),
         });
