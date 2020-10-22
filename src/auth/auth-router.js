61946a26e08f55288802594b781dbee6e9d5d59b
Adam Smith <coloradoasmith@gmail.com>
2020-10-21 18:13:49 -0600

    trying to add user id into login response

--- src/auth/auth-router.js
+++ src/auth/auth-router.js
@@ -35,6 +35,7 @@ authRouter.post("/login", jsonBodyParser, (req, res, next) => {
         const payload = { user_id: dbUser.id };
         res.send({
           authToken: AuthService.createJwt(sub, payload),
+          payload,
         });
       });
     })
